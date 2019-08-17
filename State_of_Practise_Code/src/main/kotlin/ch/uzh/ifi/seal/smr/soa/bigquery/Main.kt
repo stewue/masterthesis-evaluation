5cf3d65bd5ea86d8506d556e60687583429b9277
stewue <stefan@wuersten.ch>
2019-08-17 15:32:04 +0200

    Bigquery: clean data set from christoph

--- State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/bigquery/Main.kt
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/bigquery/Main.kt
@@ -1,27 +1,97 @@
 package ch.uzh.ifi.seal.smr.soa.bigquery
 
 import ch.uzh.ifi.seal.smr.soa.evaluation.OpenCSVWriter
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.commons.io.IOUtils
 import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
+import java.nio.charset.Charset
+
+val mapper = ObjectMapper()
+// TODO entfernen
+val token = ""
 
 fun main() {
     val inFile = File("C:\\Users\\stewue\\Downloads\\jmh-projects-bigquery-fh-201702.csv")
-    val outFile = "C:\\Users\\stewue\\Downloads\\out.csv"
+    val outFile = File("C:\\Users\\stewue\\Downloads\\out.csv").toPath()
     val res = CsvResultParser(inFile).getList()
 
-    res.forEach {
-        val url = "https://github.com/" + it.project
+    var i = 0
+    res.forEach { p ->
+        //if (i < 10) {
+        val url = "https://api.github.com/repos/" + p.project
         val obj = URL(url)
         val con = obj.openConnection() as HttpURLConnection
         con.requestMethod = "GET"
+        con.setRequestProperty("Authorization", "token $token")
 
-        println("${it.project}: ${con.responseCode}")
+        println("${p.project}: ${con.responseCode}")
 
-        it.exist = con.responseCode != 404
+        if (con.responseCode == 404) {
+            p.repoAvailable = false
+            p.selectedForStudy = false
+        } else {
+            val jsonString = IOUtils.toString(con.inputStream, Charset.defaultCharset())
+            val json = mapper.readTree(jsonString)
 
-        Thread.sleep(500)
+            val fork = json["fork"].booleanValue()
+
+            p.forked = fork
+            p.watchers = json["watchers_count"].intValue()
+            p.stars = json["stargazers_count"].intValue()
+            p.forks = json["forks_count"].intValue()
+            p.subscribers = json["subscribers_count"].intValue()
+            p.repoAvailable = true
+            p.cloneUrl = json["clone_url"].textValue()
+            p.lastUpdate = json["updated_at"].textValue()
+            p.archived = json["archived"].booleanValue()
+            p.disabled = json["disabled"].booleanValue()
+
+            if (fork) {
+                p.parentProject = json["parent"]["full_name"].textValue()
+                p.parentInList = res.any { s -> s.project == p.parentProject }
+                p.duplicatedFork = res.any { s -> s != p && s.parentProject == p.parentProject }
+                p.selectedForStudy = p.parentInList!!.not() && p.duplicatedFork!!.not()
+            } else {
+                p.selectedForStudy = true
+            }
+
+            if (p.selectedForStudy == true) {
+                p.numberOfReleases = getNumberOfReleases(p.project)
+                p.numberOfCommits = getNumberOfCommits(p.project)
+            }
+        }
+        //}
+
+        i++
     }
 
     OpenCSVWriter.write(outFile, res)
+}
+
+fun getNumberOfReleases(name: String): Int {
+    val url = "https://api.github.com/repos/$name/releases?per_page=100"
+    val obj = URL(url)
+    val con = obj.openConnection() as HttpURLConnection
+    con.requestMethod = "GET"
+    con.setRequestProperty("Authorization", "token $token")
+
+    val jsonString = IOUtils.toString(con.inputStream, Charset.defaultCharset())
+    val json = mapper.readTree(jsonString)
+
+    return json.count()
+}
+
+fun getNumberOfCommits(name: String): Int {
+    val url = "https://api.github.com/repos/$name/commits?per_page=100"
+    val obj = URL(url)
+    val con = obj.openConnection() as HttpURLConnection
+    con.requestMethod = "GET"
+    con.setRequestProperty("Authorization", "token $token")
+
+    val jsonString = IOUtils.toString(con.inputStream, Charset.defaultCharset())
+    val json = mapper.readTree(jsonString)
+
+    return json.count()
 }
--- State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/bigquery/Row.kt
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/bigquery/Row.kt
@@ -21,17 +21,41 @@ class Row {
     @CsvBindByPosition(position = 5)
     var subscribers: Int = 0
 
+    // we get a 404 error from the github api -> repo not available
     @CsvBindByPosition(position = 6)
-    var exist: Boolean = true
+    var repoAvailable: Boolean = true
+
+    // full name of the parent project (null if not a forked project)
+    @CsvBindByPosition(position = 7)
+    var parentProject: String? = null
+
+    // is the parent project in our list
+    @CsvBindByPosition(position = 8)
+    var parentInList: Boolean? = null
+
+    @CsvBindByPosition(position = 9)
+    var cloneUrl: String? = null
+
+    @CsvBindByPosition(position = 10)
+    var lastUpdate: String? = null
+
+    @CsvBindByPosition(position = 11)
+    var duplicatedFork: Boolean? = null
+
+    @CsvBindByPosition(position = 12)
+    var selectedForStudy: Boolean? = null
+
+    @CsvBindByPosition(position = 13)
+    var archived: Boolean? = null
+
+    @CsvBindByPosition(position = 14)
+    var disabled: Boolean? = null
+
+    @CsvBindByPosition(position = 15)
+    var numberOfReleases: Int? = null
+
+    @CsvBindByPosition(position = 16)
+    var numberOfCommits: Int? = null
 
     constructor()
-
-    constructor(project: String, forked: Boolean, watchers: Int, stars: Int, forks: Int, subscribers: Int) {
-        this.project = project
-        this.forked = forked
-        this.watchers = watchers
-        this.stars = stars
-        this.forks = forks
-        this.subscribers = subscribers
-    }
 }
