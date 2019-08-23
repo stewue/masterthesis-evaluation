a48fff571e3f8f706a67f9054a8411ffb1cb98fc
stewue <stefan@wuersten.ch>
2019-08-23 16:36:10 +0200

    Add jgit example

---
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/jgit/Main.kt
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.seal.smr.soa.jgit
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import java.io.File
+
+
+fun main() {
+    getRepo().use{ repository ->
+        Git(repository).use{ git ->
+            val commits = git.log().all().call()
+
+            for (commit in commits) {
+                println("LogCommit: $commit")
+            }
+            println(commits.toList().size)
+        }
+    }
+}
+
+fun getRepo(): Repository {
+    val repoDir = File("D:\\test\\.git")
+
+    val builder = FileRepositoryBuilder()
+    val repo = builder.setGitDir(repoDir)
+            .readEnvironment()
+            .findGitDir()
+            .build()
+
+    println("Having repository: " + repo.directory)
+
+    return repo
+}
