1c9edf57c0965ce42ed49e0333bd34625e953888
stewue <stefan@wuersten.ch>
2019-08-29 13:54:06 +0200

    Count @benchmarks in comments

---
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/comments/CommentVisitor.kt
@@ -0,0 +1,47 @@
+package ch.uzh.ifi.seal.smr.soa.comments
+
+import org.eclipse.jdt.core.dom.ASTVisitor
+import org.eclipse.jdt.core.dom.BlockComment
+import org.eclipse.jdt.core.dom.CompilationUnit
+import org.eclipse.jdt.core.dom.LineComment
+
+class CommentVisitor(private var compilationUnit: CompilationUnit, private val source: List<String>) : ASTVisitor() {
+    var counter = 0
+
+    override fun visit(node: LineComment): Boolean {
+
+        val startLineNumber = compilationUnit.getLineNumber(node.startPosition) - 1
+        val lineComment = source[startLineNumber].trim { it <= ' ' }
+
+        check(lineComment)
+
+        return true
+    }
+
+    override fun visit(node: BlockComment): Boolean {
+
+        val startLineNumber = compilationUnit.getLineNumber(node.startPosition) - 1
+        val endLineNumber = compilationUnit.getLineNumber(node.startPosition + node.length) - 1
+
+        val blockComment = StringBuffer()
+
+        for (lineCount in startLineNumber..endLineNumber) {
+
+            val blockCommentLine = source[lineCount].trim { it <= ' ' }
+            blockComment.append(blockCommentLine)
+            if (lineCount != endLineNumber) {
+                blockComment.append("\n")
+            }
+        }
+
+        check(blockComment.toString())
+
+        return true
+    }
+
+    private fun check(text: String) {
+        if (text.contains("@Benchmark")) {
+            counter++
+        }
+    }
+}
---
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/comments/Main.kt
@@ -0,0 +1,69 @@
+package ch.uzh.ifi.seal.smr.soa.comments
+
+import ch.uzh.ifi.seal.smr.soa.utils.CsvResultParser
+import ch.uzh.ifi.seal.smr.soa.utils.disableSystemErr
+import org.apache.logging.log4j.LogManager
+import org.eclipse.jdt.core.dom.*
+import java.io.File
+import kotlin.system.exitProcess
+
+private val log = LogManager.getLogger()
+
+fun main(args: Array<String>) {
+    disableSystemErr()
+
+    if (args.size != 2) {
+        log.error("Needed arguments: inputFile inputDir")
+        exitProcess(-1)
+    }
+
+    val inputFile = File(args[0])
+    val inputDir = args[1]
+    val outputFile = File(args[1] + "comments.csv")
+
+    val list = CsvResultParser(inputFile).getList()
+
+    list.forEach {
+        if (!it.forked) {
+            val dir = File(inputDir + "${it.project.replace('/', '#')}")
+            if (dir.exists()) {
+                log.info("Process $dir")
+                doPerProject(it.project, dir, outputFile)
+            } else {
+                log.warn("Project ${it.project} does not exist in input directory -> skipped")
+            }
+        }
+    }
+}
+
+fun doPerProject(project: String, sourceDir: File, outputFile: File) {
+    val filePaths = sourceDir.walkTopDown().filter { f ->
+        f.isFile && f.extension == "java"
+    }.map { it.absolutePath }.toList().toTypedArray()
+
+    parse(project, sourceDir, filePaths, outputFile)
+}
+
+private fun parse(project: String, sourceDirectory: File, filePaths: Array<String>, outputFile: File) {
+    val parser = ASTParser.newParser(AST.JLS11)
+    parser.setKind(ASTParser.K_COMPILATION_UNIT)
+    parser.setEnvironment(arrayOf<String>(), arrayOf(sourceDirectory.absolutePath), arrayOf("UTF-8"), true)
+    parser.setResolveBindings(true)
+    parser.setBindingsRecovery(true)
+    parser.createASTs(filePaths, null, arrayOf(), object : FileASTRequestor() {
+        override fun acceptAST(sourceFilePath: String, javaUnit: CompilationUnit) {
+            val comments = javaUnit.commentList as List<ASTNode>
+            val text = File(sourceFilePath).readLines()
+            val cv = CommentVisitor(javaUnit, text)
+
+            comments.forEach {
+                it.accept(cv)
+            }
+
+            if (cv.counter > 0) {
+                val className = sourceFilePath.substringAfter(sourceDirectory.absolutePath).substring(1)
+                outputFile.appendText("$project;$className;${cv.counter}\n")
+            }
+        }
+    }, null)
+}
