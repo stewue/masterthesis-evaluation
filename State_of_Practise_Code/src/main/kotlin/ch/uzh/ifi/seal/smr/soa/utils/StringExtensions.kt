d4e3924a2d29717be7a14d7724309a77f992914c
stewue <stefan@wuersten.ch>
2019-08-30 11:03:28 +0200

    Fix String extension

--- State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/utils/StringExtensions.kt
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/utils/StringExtensions.kt
@@ -1,7 +1,7 @@
 package ch.uzh.ifi.seal.smr.soa.utils
 
 val String.toGithubName: String
-    inline get() = this.replace(".", "/")
+    inline get() = this.replace("#", "/")
 
 val String.toFileSystemName: String
-    inline get() = this.replace("/", ".")
+    inline get() = this.replace("/", "#")
