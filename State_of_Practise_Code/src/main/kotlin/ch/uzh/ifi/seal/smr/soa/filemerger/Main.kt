7e3eb44722d47be7976760ecb770885db95c80a2
stewue <stefan@wuersten.ch>
2019-08-31 14:49:36 +0200

    Add csv filemerger

---
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/filemerger/Main.kt
@@ -0,0 +1,28 @@
+package ch.uzh.ifi.seal.smr.soa.filemerger
+
+import java.io.File
+import java.nio.channels.FileChannel
+import java.nio.file.StandardOpenOption.READ
+import java.nio.file.StandardOpenOption.WRITE
+
+fun main() {
+    val inputDirectory = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\projects\\current")
+    val mergedFile = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\out.csv")
+    mergedFile.createNewFile()
+
+    inputDirectory.walk().forEach {
+        if (it.isFile) {
+            FileChannel.open(mergedFile.toPath(), WRITE).use { dest: FileChannel ->
+                // append to destination file
+                dest.position(dest.size())
+                FileChannel.open(it.toPath(), READ).use { src: FileChannel ->
+                    var p: Long = 0
+                    val l = src.size()
+                    while (p < l) {
+                        p += src.transferTo(p, l - p, dest)
+                    }
+                }
+            }
+        }
+    }
+}
