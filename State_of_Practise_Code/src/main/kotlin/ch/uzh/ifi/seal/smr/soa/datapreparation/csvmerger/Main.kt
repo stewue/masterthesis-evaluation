5e15779ec9873a215fa1be7d533e7ce71f87ccf4
stewue <stefan@wuersten.ch>
2019-09-05 14:05:49 +0200

    Move csv merger

--- State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/datapreparation/csvmerger/Main.kt
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/datapreparation/csvmerger/Main.kt
@@ -1,4 +1,4 @@
-package ch.uzh.ifi.seal.smr.soa.utils.csvmerger
+package ch.uzh.ifi.seal.smr.soa.datapreparation.csvmerger
 
 import ch.uzh.ifi.seal.smr.soa.utils.CsvResultParser
 import ch.uzh.ifi.seal.smr.soa.utils.OpenCSVWriter
