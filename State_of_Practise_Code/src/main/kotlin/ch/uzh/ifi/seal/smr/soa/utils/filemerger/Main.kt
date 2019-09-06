24d581f540244aaaecca816f63587f9e1441241d
stewue <stefan@wuersten.ch>
2019-09-06 14:37:04 +0200

    Update file merger

--- State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/utils/filemerger/Main.kt
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/utils/filemerger/Main.kt
@@ -6,22 +6,50 @@ import java.nio.file.StandardOpenOption.READ
 import java.nio.file.StandardOpenOption.WRITE
 
 fun main() {
-    val inputDirectory = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\projects\\current")
+    /*val inputDir = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\projects\\current")
     val mergedFile = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\out.csv")
-    mergedFile.createNewFile()
 
-    inputDirectory.walk().forEach {
+    oneOutputFile(inputDir, mergedFile)*/
+
+    val inputDir = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\project-history\\results-1")
+    val outputDir = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\project-history\\out")
+
+    mergePerProject(inputDir, outputDir)
+}
+
+private fun oneOutputFile(inputDir: File, outputFile: File) {
+    outputFile.createNewFile()
+
+    inputDir.walk().forEach {
         if (it.isFile) {
-            FileChannel.open(mergedFile.toPath(), WRITE).use { dest: FileChannel ->
-                // append to destination file
-                dest.position(dest.size())
-                FileChannel.open(it.toPath(), READ).use { src: FileChannel ->
-                    var p: Long = 0
-                    val l = src.size()
-                    while (p < l) {
-                        p += src.transferTo(p, l - p, dest)
-                    }
-                }
+            merge(it, outputFile)
+        }
+    }
+}
+
+private fun mergePerProject(inputDir: File, outputDir: File) {
+    inputDir.walk().forEach {
+        if (it.isFile) {
+            println(it.nameWithoutExtension)
+            val project = it.nameWithoutExtension.dropLast(11)
+            val outputFile = File(outputDir.absolutePath + "/$project.csv")
+            if (!outputFile.exists()) {
+                outputFile.createNewFile()
+            }
+            merge(it, outputFile)
+        }
+    }
+}
+
+private fun merge(inputFile: File, outputFile: File) {
+    FileChannel.open(outputFile.toPath(), WRITE).use { dest: FileChannel ->
+        // append to destination file
+        dest.position(dest.size())
+        FileChannel.open(inputFile.toPath(), READ).use { src: FileChannel ->
+            var p: Long = 0
+            val l = src.size()
+            while (p < l) {
+                p += src.transferTo(p, l - p, dest)
             }
         }
     }
