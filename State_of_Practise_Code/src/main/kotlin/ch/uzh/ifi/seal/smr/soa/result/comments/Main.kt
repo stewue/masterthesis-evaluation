abe6220071e88f47fc5825412510fd278e8950d6
stewue <stefan@wuersten.ch>
2019-09-11 14:02:04 +0200

    Update comments

--- State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/comments/Main.kt
+++ State_of_Practise_Code/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/comments/Main.kt
@@ -1,12 +1,15 @@
 package ch.uzh.ifi.seal.smr.soa.result.comments
 
+import ch.uzh.ifi.seal.smr.soa.utils.CsvProjectParser
 import ch.uzh.ifi.seal.smr.soa.utils.CsvResultParser
 import java.io.File
 
 fun main() {
+    val projectFile = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Datasets\\preprocessed_repo_list_additional_information.csv")
     val comments = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\projects\\current-commit-comments.csv")
     val history = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\project-history\\merged.csv")
 
+    val projects = CsvProjectParser(projectFile).getList()
     val allBenchmarks = CsvResultParser(history).getList()
 
     comments.forEachLine { comment ->
@@ -15,6 +18,9 @@ fun main() {
         val find = allBenchmarks.filter { it.project == project && it.benchmarkName == fqn }
 
         val found = find.isNotEmpty()
-        println("$found;$project;$className;$benchmarkName")
+        val isMainRepo = projects.find {
+            project == it.project
+        }!!.mainRepo == true
+        println("$found;$isMainRepo;$project;$className;$benchmarkName")
     }
 }
