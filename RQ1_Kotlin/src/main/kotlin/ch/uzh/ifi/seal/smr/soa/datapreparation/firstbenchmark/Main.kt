f1bb3a87d3602b97dbd7d3e75bb9b28ca03fe2ba
stewue <stefan@wuersten.ch>
2019-09-13 11:46:25 +0200

    First benchmark found

---
+++ RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/datapreparation/firstbenchmark/Main.kt
@@ -0,0 +1,27 @@
+package ch.uzh.ifi.seal.smr.soa.datapreparation.firstbenchmark
+
+import ch.uzh.ifi.seal.smr.soa.utils.CsvProjectParser
+import ch.uzh.ifi.seal.smr.soa.utils.CsvResultParser
+import ch.uzh.ifi.seal.smr.soa.utils.OpenCSVWriter
+import java.io.File
+
+fun main() {
+    val fileHistory = File("D:\\mp\\history-all.csv")
+    val fileProjects = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Datasets\\preprocessed_repo_list_additional_information.csv")
+    val outFile = File("D:\\mp\\new.csv").toPath()
+
+    val items = CsvResultParser(fileHistory).getList()
+    val projects = CsvProjectParser(fileProjects).getList()
+
+    val firstFound = items.groupBy { it.project }
+            .map { (project, list) ->
+                project to list.map { it.commitTime!! }.min()!!
+            }
+            .toMap()
+
+    projects.forEach {
+        it.firstBenchmarkFound = firstFound[it.project]
+    }
+
+    OpenCSVWriter.write(outFile, projects)
+}
