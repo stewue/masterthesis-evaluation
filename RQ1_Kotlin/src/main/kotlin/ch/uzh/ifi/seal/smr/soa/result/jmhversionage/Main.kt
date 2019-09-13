8861dd263dc933c4bf674a497f23f8bd9eb71a08
stewue <stefan@wuersten.ch>
2019-09-13 13:54:39 +0200

    Python script version age

--- RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/jmhversionage/Main.kt
+++ RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/jmhversionage/Main.kt
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.smr.soa.result.jmhversionage
 
 import ch.uzh.ifi.seal.smr.soa.result.jmhDate
+import ch.uzh.ifi.seal.smr.soa.utils.CsvProjectParser
 import ch.uzh.ifi.seal.smr.soa.utils.CsvResultParser
 import java.io.File
 import kotlin.math.round
@@ -8,10 +9,12 @@ import kotlin.math.round
 private const val yearInSeconds = 31536000.0
 
 fun main() {
-    val file = File("D:\\mp\\current-merged-isMain.csv")
-    val items = CsvResultParser(file).getList()
+    val fileBenchmarks = File("D:\\mp\\current-merged-isMain.csv")
+    val fileProjects = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Datasets\\preprocessed_repo_list_additional_information.csv")
+    val items = CsvResultParser(fileBenchmarks).getList()
+    val projects = CsvProjectParser(fileProjects).getList()
 
-    println("project,benchmarks,time")
+    println("project,benchmarks,time,useJmhSince")
     items.filter { it.jmhVersion != null }
             .groupBy { it.project }
             .map { (_, items) ->
@@ -21,6 +24,8 @@ fun main() {
                 val jmhVersionDate = jmhDate.getValue(item.jmhVersion!!)
                 val timeDiff = round((item.commitTime!! - jmhVersionDate) / yearInSeconds * 100) / 100.0
 
-                println("${item.project},$count,$timeDiff")
+                val project = projects.find { it.project == item.project }!!
+                val useJmhSince = round((project.lastCommit!! - project.firstBenchmarkFound!!) / yearInSeconds * 100) / 100.0
+                println("${item.project},$count,$timeDiff,$useJmhSince")
             }
 }
