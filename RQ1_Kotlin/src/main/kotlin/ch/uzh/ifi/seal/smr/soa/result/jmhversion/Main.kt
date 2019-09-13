905653a290581fd34bb75fa2d3649b9de1ace87e
stewue <stefan@wuersten.ch>
2019-09-13 10:45:26 +0200

    Plot jmhVersion usage

--- RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/jmhversion/Main.kt
+++ RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/jmhversion/Main.kt
@@ -1,36 +1,31 @@
 package ch.uzh.ifi.seal.smr.soa.result.jmhversion
 
 import ch.uzh.ifi.seal.bencher.JMHVersion
-import ch.uzh.ifi.seal.smr.soa.result.jmhDate
-import ch.uzh.ifi.seal.smr.soa.result.percentageString
 import ch.uzh.ifi.seal.smr.soa.utils.CsvProjectParser
 import ch.uzh.ifi.seal.smr.soa.utils.Row
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 
 fun main() {
     val file = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Datasets\\preprocessed_repo_list_additional_information.csv")
     val items = CsvProjectParser(file).getList().filter { it.repoAvailable }
 
     jmhVersion(items)
-    println("-----")
-    jmhDate(items)
 }
 
 private fun jmhVersion(items: List<Row>) {
     val list = items
-            .groupingBy { it.jmhVersion }
-            .eachCount()
             .map {
-                val key = convertJmhVersion(it.key)
+                val key = convertJmhVersionWithoutPatch(it.jmhVersion)
                 if (key == null) {
                     null
                 } else {
-                    key to it.value
+                    key
                 }
             }
             .filterNotNull()
+            .groupingBy { it }
+            .eachCount()
+            .toList()
 
     val sorted = list.sortedWith(
             compareBy {
@@ -38,40 +33,19 @@ private fun jmhVersion(items: List<Row>) {
             }
     )
 
-    val total = sorted.map { it.second }.sum()
+    println("version,count")
     sorted.forEach { (value, count) ->
-        println("$value -> $count (${percentageString(count, total)})")
+        println("$value,$count")
     }
 }
 
-private fun jmhDate(items: List<Row>) {
-    val list = items
-            .filter { !it.jmhVersion.isNullOrBlank() }
-            .map { jmhDate.getValue(convertJmhVersion(it.jmhVersion)!!) }
-            .groupingBy { it }
-            .eachCount()
-            .toList()
-            .sortedBy { it.first }
-
-    val total = list.map { it.second }.sum()
-    list.forEach { (value, count) ->
-        println("${getDate(value)} -> $count (${percentageString(count, total)})")
-    }
-}
-
-private fun convertJmhVersion(input: String?): JMHVersion? {
+private fun convertJmhVersionWithoutPatch(input: String?): JMHVersion? {
     return if (input == null || input.isNullOrBlank()) {
         null
     } else {
         val list = input.split(".")
         val major = list[0].toInt()
         val minor = list[1].toInt()
-        val patch = list.getOrNull(2)?.toInt() ?: 0
-        JMHVersion(major, minor, patch)
+        JMHVersion(major, minor)
     }
-}
-
-private fun getDate(timestamp: Int): String {
-    val date = Date(timestamp.toLong() * 1000)
-    return SimpleDateFormat("yyyy-MM").format(date)
 }
---
+++ RQ1_Python/jmh_version.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\jmhversion.csv', dtype={'version' : 'str'})
+
+plt.bar(data['version'], data['count'])
+plt.xticks(rotation='vertical')
+plt.xlabel('JMH Version')
+plt.ylabel('Number of projects')
+plt.show()
