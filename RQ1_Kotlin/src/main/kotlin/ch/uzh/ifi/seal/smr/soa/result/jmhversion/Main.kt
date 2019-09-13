c40b70f148403bf771adb06af3a198d8dda26778
stewue <stefan@wuersten.ch>
2019-09-13 13:18:55 +0200

    Update

--- RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/jmhversion/Main.kt
+++ RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/result/jmhversion/Main.kt
@@ -2,17 +2,12 @@ package ch.uzh.ifi.seal.smr.soa.result.jmhversion
 
 import ch.uzh.ifi.seal.bencher.JMHVersion
 import ch.uzh.ifi.seal.smr.soa.utils.CsvProjectParser
-import ch.uzh.ifi.seal.smr.soa.utils.Row
 import java.io.File
 
 fun main() {
     val file = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Datasets\\preprocessed_repo_list_additional_information.csv")
-    val items = CsvProjectParser(file).getList().filter { it.repoAvailable }
+    val items = CsvProjectParser(file).getList()
 
-    jmhVersion(items)
-}
-
-private fun jmhVersion(items: List<Row>) {
     val list = items
             .filter{
                 it.mainRepo == true
