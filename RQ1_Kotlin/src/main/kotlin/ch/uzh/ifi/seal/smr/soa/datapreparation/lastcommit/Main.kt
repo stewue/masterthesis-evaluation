49f5b6a8820f570d797aadce1c5a221e1ee1131a
stewue <stefan@wuersten.ch>
2019-09-18 17:37:06 +0200

    Calculate last commit time

---
+++ RQ1_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/soa/datapreparation/lastcommit/Main.kt
@@ -0,0 +1,26 @@
+package ch.uzh.ifi.seal.smr.soa.datapreparation.lastcommit
+
+import ch.uzh.ifi.seal.smr.soa.utils.CsvProjectParser
+import ch.uzh.ifi.seal.smr.soa.utils.OpenCSVWriter
+import java.io.File
+
+fun main() {
+    val input = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\history\\history-selected-commits.csv")
+    val projectFile = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Datasets\\preprocessed_repo_list_additional_information.csv")
+    val projects = CsvProjectParser(projectFile).getList()
+
+    val map = mutableMapOf<String, Pair<Int, String>>()
+    input.forEachLine {
+        val (project, time, commitId) = it.split(";")
+        if (map[project] == null) {
+            map[project] = Pair(time.toInt(), commitId)
+        }
+    }
+
+    map.forEach { (key, value) ->
+        val project = projects.first { it.project == key }
+        project.lastCommit = value.first
+    }
+
+    OpenCSVWriter.write(projectFile.toPath(), projects)
+}
