550363a2797e211f39e36df1090bd468568e5cef
stewue <stefan@wuersten.ch>
2019-09-25 17:29:42 +0200

    Clean

--- RQ1_Python/feature_per_project_correlation.py
+++ RQ1_Python/feature_per_project_correlation.py
@@ -4,11 +4,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\featuresperproject - Kopie.csv')
-data['parametrizationUsed'] = data.apply(lambda row: row.parametrizationUsedPercentage > 0, axis=1)
-data['returnTypeOrBlackholeUsed'] = data.apply(lambda row: row.returnTypeOrBlackholeUsedPercentage > 0, axis=1)
-data['hasStateObjectsWithoutJmhParams'] = data.apply(lambda row: row.hasStateObjectsWithoutJmhParamsPercentage > 0,
-                                                     axis=1)
+data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\featuresperproject.csv')
 
 selected = data[['avgNumberOfBenchmarksPerClass', 'parametrizationUsedPercentage', 'groupsUsedPercentage', 'blackholeUsedPercentage',
                  'controlUsedPercentage', 'hasStateObjectsWithoutJmhParamsPercentage', 'returnTypeUsedPercentage', 'returnTypeOrBlackholeUsedPercentage',
