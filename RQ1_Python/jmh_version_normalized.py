e891d15409a90f8ceb1fae02a66c18b56c8a8648
stewue <stefan@wuersten.ch>
2019-10-02 17:20:21 +0200

    Jmh version normalized by month

---
+++ RQ1_Python/jmh_version_normalized.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\jmhversionnormalized.csv', dtype={'version' : 'str'})
+
+plt.bar(data['version'], data['normalizedCount'])
+plt.xticks(rotation='vertical')
+plt.xlabel('JMH version')
+plt.ylabel('# projects normalized by number of month to next release')
+plt.legend()
+plt.tight_layout()
+#plt.show()
+plt.savefig('export.png')
