1075126a4fde2fec3c8100c2ef7cd7d548795ff1
stewue <stefan@wuersten.ch>
2019-11-05 15:18:29 +0100

    Add default threshold

---
+++ RQ2_Kotlin/src/main/java/org/openjdk/jmh/runner/Defaults.java
@@ -0,0 +1,7 @@
+package org.openjdk.jmh.runner;
+
+public class Defaults {
+    public static final double RECONFIGURE_COV_THRESHOLD = 0.01;
+    public static final double RECONFIGURE_CI_THRESHOLD = 0.03;
+    public static final double RECONFIGURE_KLD_THRESHOLD = 0.99;
+}
