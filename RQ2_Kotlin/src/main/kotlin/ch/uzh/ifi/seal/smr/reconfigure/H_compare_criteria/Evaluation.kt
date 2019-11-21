701bd905f1906eadab110c0040a20e76e3b79ddd
stewue <stefan@wuersten.ch>
2019-11-21 16:20:19 +0100

    Update variability evaluation

--- RQ2_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/reconfigure/H_compare_criteria/Evaluation.kt
+++ RQ2_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/reconfigure/H_compare_criteria/Evaluation.kt
@@ -9,21 +9,19 @@ import org.openjdk.jmh.reconfigure.statistics.Sampler
 import org.openjdk.jmh.reconfigure.statistics.ci.CiPercentage
 import org.openjdk.jmh.reconfigure.statistics.ci.CiRatio
 import java.io.File
-import java.io.OutputStream
-import java.io.PrintStream
+import java.io.FileWriter
 import java.nio.file.Paths
-import java.util.*
 
 class Evaluation(private val csvInput: File, private val outputDir: String) {
 
     private val header = "project;commit;benchmark;params;threshold;f2;f3;f4;f5;reachedForks;reachedF1;reachedF2;reachedF3;reachedF4;reachedF5"
 
     fun run() {
-        disableSystemErr()
-
+        println("commit;;benchmark;params;meanDefault;meanCov;meanCi;meanKld;ciPercentageDefault;ciPercentageCov;ciPercentageCi;ciPercentageKld;effectSizeCov;effectSizeCi;effectSizeKld;wilcoxonCov;wilcoxonCi;wilcoxonKld;ratioLowerCov;ratioUpperCov;ratioLowerCi;ratioUpperCi;ratioLowerKld;ratioUpperKld")
         val inputCov = Paths.get(outputDir, "outputCovTotal.csv").toFile()
         val inputCi = Paths.get(outputDir, "outputCiTotal.csv").toFile()
         val inputDivergence = Paths.get(outputDir, "outputDivergenceTotal.csv").toFile()
+        val output = FileWriter(Paths.get(outputDir, "variability.csv").toFile())
 
         val reachedForkCov = mutableMapOf<String, Int>()
         val reachedIterationCov = mutableMapOf<String, MutableMap<Int, Int>>()
@@ -32,56 +30,9 @@ class Evaluation(private val csvInput: File, private val outputDir: String) {
         val reachedForkDivergence = mutableMapOf<String, Int>()
         val reachedIterationDivergence = mutableMapOf<String, MutableMap<Int, Int>>()
 
-        inputCov.forEachLine {
-            if (it == header) {
-                return@forEachLine
-            }
-
-            val parts = it.split(";")
-            val name = "${parts[0]}#${parts[2]}#${parts[3]}"
-            reachedForkCov[name] = parts[9].toInt()
-            reachedIterationCov[name] = mutableMapOf(
-                    1 to parts[10].toInt(),
-                    2 to parts[11].toInt(),
-                    3 to parts[12].toInt(),
-                    4 to parts[13].toInt(),
-                    5 to parts[14].toInt()
-            )
-        }
-
-        inputCi.forEachLine {
-            if (it == header) {
-                return@forEachLine
-            }
-
-            val parts = it.split(";")
-            val name = "${parts[0]}#${parts[2]}#${parts[3]}"
-            reachedForkCi[name] = parts[9].toInt()
-            reachedIterationCi[name] = mutableMapOf(
-                    1 to parts[10].toInt(),
-                    2 to parts[11].toInt(),
-                    3 to parts[12].toInt(),
-                    4 to parts[13].toInt(),
-                    5 to parts[14].toInt()
-            )
-        }
-
-        inputDivergence.forEachLine {
-            if (it == header) {
-                return@forEachLine
-            }
-
-            val parts = it.split(";")
-            val name = "${parts[0]}#${parts[2]}#${parts[3]}"
-            reachedForkDivergence[name] = parts[9].toInt()
-            reachedIterationDivergence[name] = mutableMapOf(
-                    1 to parts[10].toInt(),
-                    2 to parts[11].toInt(),
-                    3 to parts[12].toInt(),
-                    4 to parts[13].toInt(),
-                    5 to parts[14].toInt()
-            )
-        }
+        parseInput(inputCov, reachedForkCov, reachedIterationCov)
+        parseInput(inputCi, reachedForkCi, reachedIterationCi)
+        parseInput(inputDivergence, reachedForkDivergence, reachedIterationDivergence)
 
         csvInput.walk().forEach {
             if (it.isFile) {
@@ -89,40 +40,24 @@ class Evaluation(private val csvInput: File, private val outputDir: String) {
                     val name = it.nameWithoutExtension
                     val all = CsvLineParser(it).getList().map { it.getHistogramItem() }
 
+                    val (project, benchmark, params) = name.split("#")
+
+                    output.append("$project;;$benchmark;$params")
+
+
+                    // construct dynamic stopping dataset
                     val defaultItems = all.filter { it.iteration > 50 }
                     val covItems = getItems(all, reachedForkCov.getValue(name), reachedIterationCov.getValue(name))
                     val ciItems = getItems(all, reachedForkCi.getValue(name), reachedIterationCi.getValue(name))
                     val kldItems = getItems(all, reachedForkDivergence.getValue(name), reachedIterationDivergence.getValue(name))
 
-                    val ciDefault = ci(defaultItems)
-                    val ciCov = ci(covItems)
-                    val ciCi = ci(ciItems)
-                    val ciKld = ci(kldItems)
-
-                    println("${ciCov / ciDefault};${ciCi / ciDefault};${ciKld / ciDefault}")
-
+                    // sample
                     val defaultSample = Sampler(defaultItems).getSample(10000)
                     val covSample = Sampler(covItems).getSample(10000)
                     val ciSample = Sampler(ciItems).getSample(10000)
                     val kldSample = Sampler(kldItems).getSample(10000)
 
-                    val defaultSampleDouble = HistogramHelper.toArray(defaultSample)
-                    val covSampleDouble = HistogramHelper.toArray(covSample)
-                    val ciSampleDouble = HistogramHelper.toArray(ciSample)
-                    val kldSampleDouble = HistogramHelper.toArray(kldSample)
-
-                    val effectSizeCov = CliffDeltaTest(defaultSampleDouble, covSampleDouble).effectSize
-                    val effectSizeCi = CliffDeltaTest(defaultSampleDouble, ciSampleDouble).effectSize
-                    val effectSizeKld = CliffDeltaTest(defaultSampleDouble, kldSampleDouble).effectSize
-
-                    println("${Math.abs(effectSizeCov)};${Math.abs(effectSizeCi)};${Math.abs(effectSizeKld)}")
-
-                    val wilcoxonCov = WilcoxonRankSumTest(defaultSampleDouble, covSampleDouble).pValue
-                    val wilcoxonCi = WilcoxonRankSumTest(defaultSampleDouble, ciSampleDouble).pValue
-                    val wilcoxonKld = WilcoxonRankSumTest(defaultSampleDouble, kldSampleDouble).pValue
-
-                    println("$wilcoxonCov;$wilcoxonCi;$wilcoxonKld")
-
+                    // outlier detector
                     val odDefault = OutlierDetector(OUTLIER_FACTOR, defaultSample)
                     val odCov = OutlierDetector(OUTLIER_FACTOR, covSample)
                     val odCi = OutlierDetector(OUTLIER_FACTOR, ciSample)
@@ -132,6 +67,33 @@ class Evaluation(private val csvInput: File, private val outputDir: String) {
                     odCi.run()
                     odKld.run()
 
+                    val defaultSampleDouble = HistogramHelper.toArray(odDefault.inlier)
+                    val covSampleDouble = HistogramHelper.toArray(odCov.inlier)
+                    val ciSampleDouble = HistogramHelper.toArray(odCi.inlier)
+                    val kldSampleDouble = HistogramHelper.toArray(odKld.inlier)
+
+                    output.append(";${defaultSampleDouble.average()};${covSampleDouble.average()};${ciSampleDouble.average()};${kldSampleDouble.average()}")
+
+                    val ciDefault = ci(odDefault.inlier)
+                    val ciCov = ci(odCov.inlier)
+                    val ciCi = ci(odCi.inlier)
+                    val ciKld = ci(odKld.inlier)
+
+                    output.append(";$ciDefault;$ciCov;$ciCi;$ciKld")
+
+
+                    val effectSizeCov = CliffDeltaTest(defaultSampleDouble, covSampleDouble).effectSize
+                    val effectSizeCi = CliffDeltaTest(defaultSampleDouble, ciSampleDouble).effectSize
+                    val effectSizeKld = CliffDeltaTest(defaultSampleDouble, kldSampleDouble).effectSize
+
+                    output.append(";${Math.abs(effectSizeCov)};${Math.abs(effectSizeCi)};${Math.abs(effectSizeKld)}")
+
+                    val wilcoxonCov = WilcoxonRankSumTest(defaultSampleDouble, covSampleDouble).pValue
+                    val wilcoxonCi = WilcoxonRankSumTest(defaultSampleDouble, ciSampleDouble).pValue
+                    val wilcoxonKld = WilcoxonRankSumTest(defaultSampleDouble, kldSampleDouble).pValue
+
+                    output.append(";$wilcoxonCov;$wilcoxonCi;$wilcoxonKld")
+
                     val ciRatioCov = CiRatio(odDefault.inlier, odCov.inlier, 10000, 0.01, "mean", -1)
                     val ciRatioCi = CiRatio(odDefault.inlier, odCi.inlier, 10000, 0.01, "mean", -1)
                     val ciRatioKld = CiRatio(odDefault.inlier, odKld.inlier, 10000, 0.01, "mean", -1)
@@ -139,7 +101,9 @@ class Evaluation(private val csvInput: File, private val outputDir: String) {
                     ciRatioCi.run()
                     ciRatioKld.run()
 
-                    println("${ciRatioCov.lower > 1 || ciRatioCov.upper < 1};${ciRatioCi.lower > 1 || ciRatioCi.upper < 1};${ciRatioKld.lower > 1 || ciRatioKld.upper < 1}")
+                    output.append(";${ciRatioCov.lower};${ciRatioCov.upper};${ciRatioCi.lower};${ciRatioCi.upper};${ciRatioKld.lower};${ciRatioKld.upper}")
+                    output.append("")
+                    output.flush()
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
@@ -163,32 +127,25 @@ class Evaluation(private val csvInput: File, private val outputDir: String) {
     }
 
     private fun ci(items: List<HistogramItem>): Double {
-        val sample = Sampler(items).getSample(10000)
-
-        Collections.sort(sample) { item1, item2 ->
-            val diffFork = item1.fork - item2.fork
-            if (diffFork > 0) {
-                1
-            } else if (diffFork < 0) {
-                -1
-            } else {
-                val diffIteration = item1.iteration - item2.iteration
-                if (diffIteration > 0) {
-                    1
-                } else if (diffIteration < 0) {
-                    -1
-                } else {
-                    0
-                }
-            }
-        }
-
-        return CiPercentage(sample, 10000).value
+        return CiPercentage(items, 10000).value
     }
 
-    private fun disableSystemErr() {
-        System.setErr(PrintStream(object : OutputStream() {
-            override fun write(b: Int) {}
-        }))
+    private fun parseInput(input: File, reachedFork: MutableMap<String, Int>, reachedIteration: MutableMap<String, MutableMap<Int, Int>>) {
+        input.forEachLine {
+            if (it == header) {
+                return@forEachLine
+            }
+
+            val parts = it.split(";")
+            val name = "${parts[0]}#${parts[2]}#${parts[3]}"
+            reachedFork[name] = parts[9].toInt()
+            reachedIteration[name] = mutableMapOf(
+                    1 to parts[10].toInt(),
+                    2 to parts[11].toInt(),
+                    3 to parts[12].toInt(),
+                    4 to parts[13].toInt(),
+                    5 to parts[14].toInt()
+            )
+        }
     }
 }
