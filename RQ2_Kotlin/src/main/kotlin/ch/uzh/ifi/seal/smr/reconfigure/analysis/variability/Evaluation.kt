303d6fff3f13dc2345da79747ba94f6e37a9bb84
stewue <stefan@wuersten.ch>
2019-12-06 15:29:58 +0100

    Add variability evaluation

---
+++ RQ2_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/reconfigure/analysis/variability/Evaluation.kt
@@ -0,0 +1,122 @@
+package ch.uzh.ifi.seal.smr.reconfigure.analysis.variability
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val file = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ2_Results\\variability\\")
+
+    file.walk().forEach {
+        if (it.isFile && it.name == "variability.csv") {
+            val name = it.parentFile.name
+            eval(name, it)
+        }
+    }
+
+}
+
+private fun eval(name: String, file: File) {
+    var counter = 0.0
+
+    var meanChangeRateCov = mutableListOf<Double>()
+    var meanChangeRateCi = mutableListOf<Double>()
+    var meanChangeRateDivergence = mutableListOf<Double>()
+
+    var ciWidthSmallerDefault = 0
+
+    var combinationCov = 0
+    var combinationCi = 0
+    var combinationDivergence = 0
+
+    var ciRatioCov = 0
+    var ciRatioCi = 0
+    var ciRatioDivergence = 0
+
+    file.forEachLine {
+        if (it == "project;commit;benchmark;params;meanDefault;meanCov;meanCi;meanKld;ciPercentageDefault;ciPercentageCov;ciPercentageCi;ciPercentageKld;effectSizeCov;effectSizeCi;effectSizeKld;wilcoxonCov;wilcoxonCi;wilcoxonKld;ratioLowerCov;ratioUpperCov;ratioLowerCi;ratioUpperCi;ratioLowerKld;ratioUpperKld") {
+            return@forEachLine
+        }
+
+        val parts = it.split(";")
+        val meanDefault = parts[4].toDouble()
+        val meanCov = parts[5].toDouble()
+        val meanCi = parts[6].toDouble()
+        val meanKld = parts[7].toDouble()
+        val ciPercentageDefault = parts[8].toDouble()
+        val ciPercentageCov = parts[9].toDouble()
+        val ciPercentageCi = parts[10].toDouble()
+        val ciPercentageKld = parts[11].toDouble()
+        val effectSizeCov = parts[12].toDouble()
+        val effectSizeCi = parts[13].toDouble()
+        val effectSizeKld = parts[14].toDouble()
+        val wilcoxonCov = parts[15].toDouble()
+        val wilcoxonCi = parts[16].toDouble()
+        val wilcoxonKld = parts[17].toDouble()
+        val ratioLowerCov = parts[18].toDouble()
+        val ratioUpperCov = parts[19].toDouble()
+        val ratioLowerCi = parts[20].toDouble()
+        val ratioUpperCi = parts[21].toDouble()
+        val ratioLowerKld = parts[22].toDouble()
+        val ratioUpperKld = parts[23].toDouble()
+
+        meanChangeRateCov.add(meanChangeRate(meanDefault, meanCov))
+        meanChangeRateCi.add(meanChangeRate(meanDefault, meanCi))
+        meanChangeRateDivergence.add(meanChangeRate(meanDefault, meanKld))
+
+        val ciPrecentageThreshold = 0.03
+        if (ciPercentageDefault < ciPrecentageThreshold) {
+            ciWidthSmallerDefault++
+        }
+
+        val effectSizeThreshold = 0.147
+        val pvalue = 0.01
+        if (effectSizeCov > effectSizeThreshold && wilcoxonCov < pvalue) {
+            combinationCov++
+        }
+        if (effectSizeCi > effectSizeThreshold && wilcoxonCi < pvalue) {
+            combinationCi++
+        }
+        if (effectSizeKld > effectSizeThreshold && wilcoxonKld < pvalue) {
+            combinationDivergence++
+        }
+
+        if (ciRatioPercentage(ratioLowerCov, ratioUpperCov) > 0.01) {
+            ciRatioCov++
+        }
+        if (ciRatioPercentage(ratioLowerCi, ratioUpperCi) > 0.01) {
+            ciRatioCi++
+        }
+        if (ciRatioPercentage(ratioLowerKld, ratioUpperKld) > 0.01) {
+            ciRatioDivergence++
+        }
+
+        counter++
+    }
+
+    val avgChangeRateCov = meanChangeRateCov.average()
+    val avgChangeRateCi = meanChangeRateCi.average()
+    val avgChangeRateDivergence = meanChangeRateDivergence.average()
+
+    print("$name;$counter")
+    print(";$avgChangeRateCov;$avgChangeRateCi;$avgChangeRateDivergence")
+    print(";${ciWidthSmallerDefault / counter}")
+    print(";${combinationCov / counter};${combinationCi / counter};${combinationDivergence / counter}")
+    println(";${ciRatioCov / counter};${ciRatioCi / counter};${ciRatioDivergence / counter}")
+}
+
+
+private fun meanChangeRate(before: Double, after: Double): Double {
+    return if ((after / before).isNaN()) {
+        0.0
+    } else {
+        abs((after / before) - 1)
+    }
+}
+
+private fun ciRatioPercentage(lower: Double, upper: Double): Double {
+    return when {
+        lower > 1 -> lower - 1
+        upper < 1 -> 1 - upper
+        else -> 0.0
+    }
+}
