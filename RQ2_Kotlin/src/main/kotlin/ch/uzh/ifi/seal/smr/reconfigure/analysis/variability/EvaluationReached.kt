8ecedf774cbfd5e8d411bf2a45f9b94bc46a9974
stewue <stefan@wuersten.ch>
2019-12-30 08:46:44 +0100

    Evaluate if there is a difference between stable and unstable benchmarks

---
+++ RQ2_Kotlin/src/main/kotlin/ch/uzh/ifi/seal/smr/reconfigure/analysis/variability/EvaluationReached.kt
@@ -0,0 +1,89 @@
+package ch.uzh.ifi.seal.smr.reconfigure.analysis.variability
+
+import ch.uzh.ifi.seal.smr.reconfigure.utils.std
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val file = File("C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ2_Results\\variability\\")
+
+    val reachedForks = mutableMapOf<String, Int>()
+    val changeRate = mutableMapOf<String, Double>()
+
+    file.walk().forEach {
+        if (it.isFile && it.name == "outputCiTotal.csv" && it.parent != file.absolutePath) {
+            reachedForks.putAll(forks(it))
+        }
+    }
+
+    file.walk().forEach {
+        if (it.isFile && it.name == "variability.csv" && it.parent != file.absolutePath) {
+            changeRate.putAll(changeRate(it))
+        }
+    }
+
+    val reached = mutableListOf<Double>()
+    val notReached = mutableListOf<Double>()
+
+    changeRate.forEach { (key, value) ->
+        val notReachedAfter5Forks = reachedForks.getValue(key) == Int.MAX_VALUE
+
+        if(notReachedAfter5Forks){
+            notReached.add(value)
+        }else{
+            reached.add(value)
+        }
+    }
+
+    println("Reached avg: ${reached.average()} +/- ${reached.std()}")
+    println("Not reached avg: ${notReached.average()} +/- ${notReached.std()}")
+}
+
+private fun forks(file: File): Map<String, Int> {
+    val ret = mutableMapOf<String, Int>()
+    file.forEachLine {
+        if (it == "project;commit;benchmark;params;threshold;f2;f3;f4;f5;reachedForks;reachedF1;reachedF2;reachedF3;reachedF4;reachedF5") {
+            return@forEachLine
+        }
+
+        val parts = it.split(";")
+        val project = parts[0]
+        val benchmark = parts[2]
+        val params = parts[3]
+        val reachedForks = parts[9].toInt()
+
+        ret["$project#$benchmark#$params"] = reachedForks
+    }
+
+    return ret
+}
+
+private fun changeRate(file: File): Map<String, Double> {
+    val ret = mutableMapOf<String, Double>()
+
+    file.forEachLine {
+        if (it == "project;commit;benchmark;params;meanDefault;meanCov;meanCi;meanKld;ciPercentageDefault;ciPercentageCov;ciPercentageCi;ciPercentageKld;effectSizeCov;effectSizeCi;effectSizeKld;wilcoxonCov;wilcoxonCi;wilcoxonKld;ratioLowerCov;ratioUpperCov;ratioLowerCi;ratioUpperCi;ratioLowerKld;ratioUpperKld") {
+            return@forEachLine
+        }
+
+        val parts = it.split(";")
+        val project = parts[0]
+        val benchmark = parts[2]
+        val params = parts[3]
+        val meanDefault = parts[4].toDouble()
+        val meanCi = parts[6].toDouble()
+
+        ret["$project#$benchmark#$params"] = meanChangeRate(meanDefault, meanCi)
+    }
+
+    return ret
+}
+
+
+private fun meanChangeRate(before: Double, after: Double): Double {
+    return if ((after / before).isNaN()) {
+        0.0
+    } else {
+        abs((after / before) - 1)
+    }
+}
