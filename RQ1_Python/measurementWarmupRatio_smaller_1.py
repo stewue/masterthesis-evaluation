fa4d25c771132f1b18968f180fabbc49069f4ed8
stewue <stefan@wuersten.ch>
2020-01-16 18:14:52 +0100

    Measurement warmup ratio modify visualization

--- RQ1_Python/measurementWarmupRatio_smaller_1.py
+++ RQ1_Python/measurementWarmupRatio_smaller_1.py
@@ -7,18 +7,30 @@ data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterar
 data['measurementWarmupRatio'] = data['measurementWarmupRatio'].apply(pd.to_numeric, errors='coerce')
 filtered = data[data['onlyModeChanged'] == False]
 filtered = filtered[filtered['onlySingleShot'] == False]
+filtered = filtered[filtered['measurementWarmupRatio'] < 1]
 
-smaller = filtered[filtered['measurementWarmupRatio'] < 1]
-smallerArray = smaller['measurementWarmupRatio']
-smallerAll, base = np.histogram(smallerArray, bins=10000, range=[0,1], weights=np.ones(len(smallerArray)) / len(smallerArray))
-cumulativeAll = np.cumsum(smallerAll)
-plt.plot(base[:-1], cumulativeAll, label="all")
+array = filtered['warmupTime'] / filtered['executionTime']
+
+all, base = np.histogram(array, bins=600, range=[0.5, 1], weights=np.ones(len(array)) / len(array))
+cumulative = np.cumsum(all)
+
+fig = plt.figure()
+total = len(array)
+
+# absolute
+ax1 = fig.add_subplot()
+ax1.plot(base[:-1], cumulative * total)
+ax1.set_ylabel('# benchmarks')
+
+# relative
+ax2 = ax1.twinx()
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1, 0))
+ax2.plot(base[:-1], cumulative)
+ax2.set_ylabel('# benchmarks [cumulative %]')
 
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-plt.ylabel('cumulative probability')
-plt.xlabel('$\mathit{measurementWarmupRatio}$')
+plt.gca().xaxis.set_major_formatter(PercentFormatter(1))
+ax1.set_xlabel('$\mathit{warmupProportion}$')
 plt.tight_layout()
 #plt.show()
-plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\measurementWarmupRatio_smaller_1.pdf')
-
-print(len(smallerArray))
+plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\warmupProportion_smaller_1.pdf')
