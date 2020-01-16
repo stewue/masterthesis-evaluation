9f2dbf5be43970871fb27738a856706a8c923de7
stewue <stefan@wuersten.ch>
2020-01-16 18:32:39 +0100

    Update cummulative execution time

--- RQ1_Python/execution_time_cumulative_larger_1.py
+++ RQ1_Python/execution_time_cumulative_larger_1.py
@@ -6,19 +6,34 @@ from matplotlib.ticker import PercentFormatter
 data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\executiontime.csv')
 
 filtered = data[data['executionTimePercentage'] > 1]
-all = filtered['executionTimePercentage']
+all = filtered['executionTimePercentage'] - 1
 
-valuesAll, base = np.histogram(all, bins=100000, range=[1, 11000], weights=np.ones(len(all)) / len(all))
+valuesAll, base = np.histogram(all, bins=100000, range=[0, 11000], weights=np.ones(len(all)) / len(all))
 cumulativeAll = np.cumsum(valuesAll)
-plt.plot(base[:-1], cumulativeAll, label="all")
+
+fig = plt.figure()
+total = len(all)
+
+# absolute
+ax1 = fig.add_subplot()
+ax1.plot(base[:-1], cumulativeAll * total)
+ax1.set_ylabel('# benchmarks')
+ax1.set_xscale('log')
+
+# relative
+ax2 = ax1.twinx()
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1, 0))
+plt.plot(base[:-1], cumulativeAll)
+ax2.set_ylabel('# benchmarks [cumulative %]')
+ax2.set_xscale('log')
 
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-plt.xlabel('$\mathit{executionTimeRatio}$')
-plt.ylabel('cumulative probability')
-plt.xscale('log')
+ax1.set_xlabel('change rate of the execution time')
+plt.xticks([0.1, 1, 10, 100, 1000, 10000], ["+10%", "+100%", "+1'000%", "+10'000%", "+100'000%", "+1'000'000%"])
 plt.tight_layout()
 #plt.show()
 plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\execution_time_cumulative_larger_1.pdf')
 
 l10 = all[all > 10]
 print(">10: " + str(l10.size / all.size))
+print(">10: " + str(l10.size))
--- RQ1_Python/execution_time_cumulative_smaller_1.py
+++ RQ1_Python/execution_time_cumulative_smaller_1.py
@@ -10,18 +10,32 @@ all = filtered['executionTimePercentage']
 
 valuesAll, base = np.histogram(all, bins=1000, range=[0, 1], weights=np.ones(len(all)) / len(all))
 cumulativeAll = np.cumsum(valuesAll)
-plt.plot(base[:-1], cumulativeAll, label="all")
+
+fig = plt.figure()
+total = len(all)
+
+# absolute
+ax1 = fig.add_subplot()
+ax1.plot(base[:-1], cumulativeAll * total)
+ax1.set_ylabel('# benchmarks')
+
+# relative
+ax2 = ax1.twinx()
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1, 0))
+plt.plot(base[:-1], cumulativeAll)
+ax2.set_ylabel('# benchmarks [cumulative %]')
 
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
-plt.xlabel('$\mathit{executionTimeRatio}$')
-plt.ylabel('cumulative probability')
-plt.xticks(np.arange(0, 1.1, step=0.1))
+ax1.set_xlabel('change rate of the execution time')
+plt.xticks([0, 0.25, 0.5, 0.75, 1], ["-100%", "-75%", "-50%", "-25%", "0%"])
 plt.tight_layout()
 #plt.show()
 plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\execution_time_cumulative_smaller_1.pdf')
 
 s4 = all[all < 0.25]
 print("<0.25: " + str(s4.size / all.size))
+print("<0.25: " + str(s4.size))
 
 s10 = all[all < 0.1]
 print("<0.1: " + str(s10.size / all.size))
+print("<0.1: " + str(s10.size))
