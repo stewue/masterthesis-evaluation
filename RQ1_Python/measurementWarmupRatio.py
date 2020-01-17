62a000bc1e9a791ed213dcbbb942d694c82d48eb
stewue <stefan@wuersten.ch>
2020-01-17 14:59:00 +0100

    Update warmup proportion

--- RQ1_Python/measurementWarmupRatio.py
+++ RQ1_Python/measurementWarmupRatio.py
@@ -6,20 +6,19 @@ from matplotlib.ticker import PercentFormatter
 data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\executiontime.csv')
 data['measurementWarmupRatio'] = data['measurementWarmupRatio'].apply(pd.to_numeric, errors='coerce')
 filtered = data[data['onlyModeChanged'] == False]
-filtered = filtered[filtered['onlySingleShot'] == False]
-filtered = filtered[filtered['measurementWarmupRatio'] > 1]
+changed = filtered[filtered['onlySingleShot'] == False]
 
-array = filtered['warmupTime'] / filtered['executionTime']
+array = changed['warmupTime'] / changed['executionTime']
 
-all, base = np.histogram(array, bins=600, range=[0, 0.5], weights=np.ones(len(array)) / len(array))
+all, base = np.histogram(array, bins=600, range=[0, 1], weights=np.ones(len(array)) / len(array))
 cumulative = np.cumsum(all)
 
 fig = plt.figure()
-total = len(array)
+totalAll = changed.shape[0]
 
 # absolute
 ax1 = fig.add_subplot()
-ax1.plot(base[:-1], cumulative * total)
+ax1.plot(base[:-1], cumulative * totalAll)
 ax1.set_ylabel('# benchmarks')
 
 # relative
@@ -33,8 +32,16 @@ plt.gca().xaxis.set_major_formatter(PercentFormatter(1))
 ax1.set_xlabel('$\mathit{warmupProportion}$')
 plt.tight_layout()
 #plt.show()
-plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\warmupProportion_larger_1.pdf')
+plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\warmupProportion.pdf')
+
+s1 = array[np.logical_and(array > 0.66, array < 0.67)]
+print("=66%: " + str(s1.size / totalAll))
+print("=66%: " + str(s1.size))
+
+s2 = array[np.logical_and(array > 0.90, array < 0.91)]
+print("=90%: " + str(s2.size / totalAll))
+print("=90%: " + str(s2.size))
 
 s1 = array[array <= 0.2]
-print("<20%: " + str(s1.size / array.size))
+print("<20%: " + str(s1.size / totalAll))
 print("<20%: " + str(s1.size))
