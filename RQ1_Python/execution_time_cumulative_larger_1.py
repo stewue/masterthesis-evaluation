015beff11255c851616b4b5a21eea493a1896343
stewue <stefan@wuersten.ch>
2020-01-18 14:19:45 +0100

    Update execution time cumulative

--- RQ1_Python/execution_time_cumulative_larger_1.py
+++ RQ1_Python/execution_time_cumulative_larger_1.py
@@ -5,35 +5,43 @@ from matplotlib.ticker import PercentFormatter
 
 data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\executiontime.csv')
 
-filtered = data[data['executionTimePercentage'] > 1]
-all = filtered['executionTimePercentage'] - 1
+filtered = data[data['onlyModeChanged'] == False]
+changed = filtered[filtered['onlySingleShot'] == False]
+changed2 = changed[changed['executionTimePercentage'] > 1]
+all = changed2['executionTimePercentage'] - 1
 
-valuesAll, base = np.histogram(all, bins=100000, range=[0, 11000], weights=np.ones(len(all)) / len(all))
+valuesAll, base = np.histogram(all, bins=100000, range=[0.1, 11000], weights=np.ones(len(all)) / len(all))
 cumulativeAll = np.cumsum(valuesAll)
 
 fig = plt.figure()
-total = len(all)
+totalAll = changed.shape[0]
+totalSelected = len(all)
+
+proportion = totalSelected / totalAll
 
 # absolute
 ax1 = fig.add_subplot()
-ax1.plot(base[:-1], cumulativeAll * total)
+ax1.plot(base[:-1], cumulativeAll * totalSelected)
 ax1.set_ylabel('# benchmarks')
 ax1.set_xscale('log')
 
 # relative
 ax2 = ax1.twinx()
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1, 0))
-plt.plot(base[:-1], cumulativeAll)
+plt.plot(base[:-1], cumulativeAll * proportion)
 ax2.set_ylabel('# benchmarks [cumulative %]')
 ax2.set_xscale('log')
 
-plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
 ax1.set_xlabel('change rate of the execution time')
 plt.xticks([0.1, 1, 10, 100, 1000, 10000], ["+10%", "+100%", "+1'000%", "+10'000%", "+100'000%", "+1'000'000%"])
 plt.tight_layout()
 #plt.show()
 plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\execution_time_cumulative_larger_1.pdf')
 
-l10 = all[all > 10]
-print(">10: " + str(l10.size / all.size))
+l1 = all[all > 0]
+print(">1: " + str(l1.size / totalAll))
+print(">1: " + str(l1.size))
+
+l10 = all[all > 9]
+print(">10: " + str(l10.size / totalAll))
 print(">10: " + str(l10.size))
--- RQ1_Python/execution_time_cumulative_smaller_1.py
+++ RQ1_Python/execution_time_cumulative_smaller_1.py
@@ -5,37 +5,45 @@ from matplotlib.ticker import PercentFormatter
 
 data = pd.read_csv('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\aggregated\\executiontime.csv')
 
-filtered = data[data['executionTimePercentage'] < 1]
-all = filtered['executionTimePercentage']
+filtered = data[data['onlyModeChanged'] == False]
+changed = filtered[filtered['onlySingleShot'] == False]
+changed2 = changed[changed['executionTimePercentage'] < 1]
+all = changed2['executionTimePercentage']
 
 valuesAll, base = np.histogram(all, bins=1000, range=[0, 1], weights=np.ones(len(all)) / len(all))
 cumulativeAll = np.cumsum(valuesAll)
 
 fig = plt.figure()
-total = len(all)
+totalAll = changed.shape[0]
+totalSelected = len(all)
+proportion = totalSelected / totalAll
 
 # absolute
 ax1 = fig.add_subplot()
-ax1.plot(base[:-1], cumulativeAll * total)
+ax1.plot(base[:-1], (1 - cumulativeAll[::-1]) * totalSelected)
 ax1.set_ylabel('# benchmarks')
 
 # relative
 ax2 = ax1.twinx()
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1, 0))
-plt.plot(base[:-1], cumulativeAll)
+plt.plot(base[:-1], (1 - cumulativeAll[::-1]) * proportion)
 ax2.set_ylabel('# benchmarks [cumulative %]')
 
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
 ax1.set_xlabel('change rate of the execution time')
-plt.xticks([0, 0.25, 0.5, 0.75, 1], ["-100%", "-75%", "-50%", "-25%", "0%"])
+plt.xticks([0, 0.25, 0.5, 0.75, 1], ["0%", "-25%", "-50%", "-75%", "-100%"])
 plt.tight_layout()
 #plt.show()
 plt.savefig('C:\\Users\\stewue\\OneDrive - Wuersten\\Uni\\19_HS\\Masterarbeit\\Repo\\Evaluation\\RQ1_Results\\images\\execution_time_cumulative_smaller_1.pdf')
 
+s1 = all[all < 1]
+print("<1: " + str(s1.size / totalAll))
+print("<1: " + str(s1.size))
+
 s4 = all[all < 0.25]
-print("<0.25: " + str(s4.size / all.size))
+print("<0.25: " + str(s4.size / totalAll))
 print("<0.25: " + str(s4.size))
 
 s10 = all[all < 0.1]
-print("<0.1: " + str(s10.size / all.size))
+print("<0.1: " + str(s10.size / totalAll))
 print("<0.1: " + str(s10.size))
